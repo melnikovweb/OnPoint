8fb6bdac7faa81bad42bc9bac9ef8496a46931a6
Andrew Melnikov <melnikovweb@gmail.com>
2022-12-12 03:46:31 +0200

    add_js_tasks

--- OnPoint_test_task/assets/js/scripts.js
+++ OnPoint_test_task/assets/js/scripts.js
@@ -13,3 +13,56 @@ window.addEventListener('scroll', function () {
     remove_class_on_scroll()
   }
 })
+
+function abc(a, b, c) {
+  return a + b + c
+}
+
+function abcdef(a, b, c, d, e, f) {
+  return a + b + c + d + e + f
+}
+
+function curry(...arg) {
+  const func = this
+
+  if (arg.length !== func.length) {
+    return curry.bind(func, ...arg)
+  }
+
+  return func(...arg)
+}
+
+Object.defineProperty(Function.prototype, 'curry', {
+  value: curry,
+})
+
+console.log(
+  abc.curry('A')('B')('C'), // 'ABC'
+  abc.curry('A', 'B')('C'), // 'ABC'
+  abc.curry('A', 'B', 'C'), // 'ABC'
+)
+console.log(
+  abcdef.curry('A')('B')('C')('D')('E')('F'), // 'ABCDEF'
+  abcdef.curry('A', 'B', 'C')('D', 'E', 'F'), // 'ABCDEF'
+)
+
+function drawRating(vote) {
+  if (vote < 0) {
+    return 'NaN'
+  } else if (vote <= 20) {
+    return '★☆☆☆☆'
+  } else if (vote <= 40) {
+    return '★★☆☆☆'
+  } else if (vote <= 60) {
+    return '★★★☆☆'
+  } else if (vote <= 80) {
+    return '★★★★☆'
+  } else if (vote <= 100) {
+    return '★★★★★'
+  }
+}
+
+console.log(drawRating(0)) // ★☆☆☆☆
+console.log(drawRating(1)) // ★☆☆☆☆
+console.log(drawRating(50)) // ★★★☆☆
+console.log(drawRating(99)) // ★★★★★
